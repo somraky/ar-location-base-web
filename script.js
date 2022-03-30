0b01106a1d531f606450143e5c121475828696bc
somraky <somrak.yimsara@gmail.com>
2022-03-31 02:19:24 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,27 +1,14 @@
 window.addEventListener("load", () => {
-    console.log("load");
+    console.log("run script");
     const text = document.getElementById("text");
-    const entity = document.getElementById("entity");
-    entity.addEventListener("gps-entity-place-update-positon", (event) => {
-        console.log("gps-entity-place-update-positon");
-        console.log("distanceMsg", entity.getAttribute("distanceMsg"));
-        // document.getElementById(
-        //     "debug"
-        // ).textContent = `${event.detail.distance}m`;
-        // text.setAttribute(
-        //     "value",
-        //     entity.getAttribute("distanceMsg") + " left"
-        // );
-    });
-
-    const gpscamera = document.getElementById("gpscamera");
-    gpscamera.addEventListener("gps-camera-update-positon", (event) => {
-        console.log("cam position", event.detail.position);
+    text.addEventListener("gps-entity-place-update-position", (event) => {
+        document.getElementById(
+            "debug"
+        ).textContent = `${event.detail.distance}m`;
         text.setAttribute(
             "value",
-            "cam position" + event.detail.position
+            text.getAttribute("distanceMsg") + " left"
         );
     });
-})
-
+});
 
