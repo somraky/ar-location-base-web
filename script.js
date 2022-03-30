772b85b46107a47f3995f65e6dd0419057b8164e
somraky <somrak.yimsara@gmail.com>
2022-03-31 01:11:22 +0700

    test gpscamera

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-window.onload = () => {
-    // window.addEventListener("load", () => {
+window.addEventListener("load", () => {
+    console.log("load");
     const text = document.getElementById("text");
     const entity = document.getElementById("entity");
     entity.addEventListener("gps-entity-place-update-positon", (event) => {
         console.log("gps-entity-place-update-positon");
         console.log("distanceMsg", entity.getAttribute("distanceMsg"));
-        document.getElementById(
-            "debug"
-        ).textContent = `${event.detail.distance}m`;
+        // document.getElementById(
+        //     "debug"
+        // ).textContent = `${event.detail.distance}m`;
+        // text.setAttribute(
+        //     "value",
+        //     entity.getAttribute("distanceMsg") + " left"
+        // );
+    });
+
+    const gpscamera = document.getElementById("gpscamera");
+    gpscamera.addEventListener("gps-camera-update-positon", (event) => {
+        console.log("cam position", event.detail.position);
         text.setAttribute(
             "value",
-            entity.getAttribute("distanceMsg") + " left"
+            "cam position" + event.detail.position
         );
     });
-    // });
-};
+})
+
+
