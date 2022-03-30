8da273e3071c20dd2e698b988fde6bc2ef2619bb
somraky <somrak.yimsara@gmail.com>
2022-03-31 00:36:37 +0700

    update

--- script.js
+++ script.js
@@ -1,40 +1,17 @@
 window.onload = () => {
-    let places = staticLoadPlaces();
-    renderPlaces(places);
-};
-
-function staticLoadPlaces() {
-    return [
-        {
-            name: 'Magnemite',
-            location: {
-                lat: 15.9035343,
-                lng: 100.2486177,
-                // lat: 15.90355871,
-                // lng: 100.24873441,
-            }
-        },
-    ];
-}
-
-function renderPlaces(places) {
-    let scene = document.querySelector('a-scene');
-
-    places.forEach((place) => {
-        let latitude = place.location.lat;
-        let longitude = place.location.lng;
-
-        let model = document.createElement('a-entity');
-        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('gltf-model', './ARModels/magnemite/scene.gltf');
-        model.setAttribute('rotation', '0 180 0');
-        // model.setAttribute('animation-mixer', '');
-        model.setAttribute('scale', '0.4 0.4 0.4');
-
-        model.addEventListener('loaded', () => {
-            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-        });
-
-        scene.appendChild(model);
+    // window.addEventListener("load", () => {
+    const text = document.getElementById("text");
+    const entity = document.getElementById("entity");
+    entity.addEventListener("gps-entity-place-update-positon", (event) => {
+        console.log("gps-entity-place-update-positon");
+        console.log("distanceMsg", entity.getAttribute("distanceMsg"));
+        document.getElementById(
+            "debug"
+        ).textContent = `${event.detail.distance}m`;
+        text.setAttribute(
+            "value",
+            entity.getAttribute("distanceMsg") + " left"
+        );
     });
-}
+    // });
+};
